04b148f254ad73847829bb60db2c6b2a5a5214ea
bekhruzAbdukodirov <128704303+bekhruzAbdukodirov@users.noreply.github.com>
2023-05-09 23:20:31 +0500

    Add files via upload

---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+
+import ExpensesChart from "./ExpensesChart";
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    // console.log('Expenses.js')
+    // console.log(selectedYear)
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} /> 
+        <ExpensesList items={filteredExpenses}/>
+
+        {/* <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      /> */}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
